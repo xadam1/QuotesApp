cdce713aa0b14e5ff13ced93aa027329218805ac
xadam1 <49945447+xadam1@users.noreply.github.com>
2020-02-04 20:28:41 +0100

    Uploading to git from mac

---
+++ QuotesApp/QuotesPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace QuotesApp
+{
+    public partial class QuotesPage : ContentPage
+    {
+        private string[] quotes = {
+            "Be yourself, everyone else is already taken.",
+            "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+            "In three words I can sum up everything I've learned about life: it goes on.",
+            "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+            "Always forgive your enemies; nothing annoys them so much."
+        };
+
+        private int quoteNum;
+
+
+        public QuotesPage()
+        {
+            InitializeComponent();
+
+            Padding = new Thickness(20);
+
+            currentQuote.Text = quotes[quoteNum];
+        }
+
+        public void BtnClicked(object sender, EventArgs e)
+        {
+            quoteNum++;
+            if (quoteNum >= quotes.Length)
+            {
+                quoteNum = 0;
+            }
+
+            currentQuote.Text = quotes[quoteNum];
+
+        }
+
+    }
+}
